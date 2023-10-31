28b2ec7cb644ffb0c7d260601b3b4de1479e056e
Bohdan <thorykbv@gmail.com>
2023-10-31 20:23:50 +0200

    Th beginning of the project

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap";
+function App() {
+  return (
+    <div className="text-center mt-5"><h1>Web App</h1></div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+import * as ReactDOMClient from "react-dom/client";
+import App from "./App";
+const app = ReactDOMClient.createRoot(document.getElementById("root"));
+app.render(<App />);
